1dc91fa50f3b725f1632cf486ad23e9b7ff10606
eliya levi <eliya0543@gmail.com>
2025-06-29 12:35:24 +0300

    new functions

--- debuug.py
+++ debuug.py
@@ -17,39 +17,20 @@ def sum_lists(list1):
     :return: sum of list1
     """
     sum_list = 0
+    amount = 0
     for number in list1:
         sum_list += number
-    return sum_list
-
-
-def amount_in_list(list1):
-    """
-    this function gets a list
-    :param list1: list to count
-    :return: amount of list1
-    """
-    amount = 0
-    for amount in list1:
         amount += 1
-    return amount
-
-def average_lists(sum_list, amount):
-    """
-    this function gets the average of list1
-    :param sum_list: list to average
-    :param amount: amount of param in list1
-    :return: average of list1
-    """
     average = sum_list / amount
     print(f"the average is:  {average}")
     return average
 
-def main():
-    """
 
-    :return:
-    """
-    average_lists(sum_lists(combine_lists([1, 2, 3, 4, 5], [6, 7, 8, 9])), amount_in_list(combine_lists([1, 2, 3, 4, 5], [6, 7, 8, 9])))
+def main():
+    list1 = [1, 2, 3 ,4 ,5]
+    list2 = [6, 7, 8, 9, 10]
+    sum_lists(combine_lists(list1, list2))
+
 
 if __name__ == '__main__':
     main()
